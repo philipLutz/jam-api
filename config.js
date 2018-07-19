3985ec6aea02948d4ff67cd38d33dcafe3cd61b4
Philip Lutz <philiplutz413@gmail.com>
2018-07-18 17:57:38 -0700

    fixed cors

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/jam-api';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-jam-api';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'phil';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
